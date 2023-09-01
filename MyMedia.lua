5a6cb8a297a569901ce9dcdd7c020e1277831a5c
tony-taylor <tony-taylor@users.noreply.github.com>
2023-09-01 04:34:43 +0000

    Regenerated Lua

--- MyMedia.lua
+++ MyMedia.lua
@@ -15,6 +15,7 @@ LSM:Register("font", "*.ttf", [[Interface/Addons/UncouthMedia/font/*.ttf]])
 --   SOUND
 -- -----
 LSM:Register("sound", "Decidedly_Uncouth.mp3", [[Interface/Addons/UncouthMedia/sound/Decidedly_Uncouth.mp3]])
+LSM:Register("sound", "so_brave.ogg", [[Interface/Addons/UncouthMedia/sound/so_brave.ogg]])
 -- -----
 --   STATUSBAR
 -- -----
