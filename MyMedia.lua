f015d8b209eb499d5e726a84bd91c5ad0c83b79a
tony-taylor <tony-taylor@users.noreply.github.com>
2023-09-03 18:02:43 +0000

    Regenerated Lua

--- MyMedia.lua
+++ MyMedia.lua
@@ -2,21 +2,21 @@ local LSM = LibStub("LibSharedMedia-3.0")
 -- -----
 -- BACKGROUND
 -- -----
-LSM:Register("background", "Uncouth: *", [[Interface/Addons/UncouthMedia/background/*]])
+--       No background files found.
 -- -----
 --  BORDER
 -- -----
-LSM:Register("border", "Uncouth: *", [[Interface/Addons/UncouthMedia/border/*]])
+--       No border files found.
 -- -----
 --   FONT
 -- -----
-LSM:Register("font", "Uncouth: *", [[Interface/Addons/UncouthMedia/font/*.ttf]])
+--       No font files found.
 -- -----
 --   SOUND
 -- -----
-LSM:Register("sound", "Uncouth: hello darkness", [[Interface/Addons/UncouthMedia/sound/hello_darkness.ogg]])
-LSM:Register("sound", "Uncouth: so brave", [[Interface/Addons/UncouthMedia/sound/so_brave.ogg]])
+LSM:Register("sound", "Uncouth: Hello Darkness", [[Interface/Addons/UncouthMedia/sound/Hello_Darkness.ogg]])
+LSM:Register("sound", "Uncouth: So Brave", [[Interface/Addons/UncouthMedia/sound/So_Brave.ogg]])
 -- -----
 --   STATUSBAR
 -- -----
-LSM:Register("statusbar", "Uncouth: *", [[Interface/Addons/UncouthMedia/statusbar/*]])
+--       No statusbar files found.
